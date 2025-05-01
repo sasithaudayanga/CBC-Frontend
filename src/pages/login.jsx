3db4200597419bdcdec380163807e48380c751c4
Sasitha <sasitha.rathnayake@gmail.com>
2025-05-01 13:06:23 +0530

    login page modified

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,52 @@
-export default function LoginPage(){
-    return(
-        <div className="w-full h-screen flex flex-col justify-evenly items-center ">
-            <h1 className="text-blue-800 font-bold text-4xl text-center">LogIn</h1>
-            <p className="text-blue-700 font-sans font-style-oblique text-1xl text-center">Everything Under One Space</p>
-            <button className="bg-blue-800 rounded-md text-white w-[70px] h-[30px]">Login</button>
+import { useState } from "react"
 
+export default function LoginPage() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleLogin() {
+        console.log(email)
+        console.log(password)
+    }
+
+    return (
+        <div className="w-full  bg-[url('/loginbg.jpg')] bg-center bg-cover h-screen flex justify-evenly items-center ">
+            <div className="w-[50%] h-full bg-white">
+
+            </div>
+
+            <div className="w-[50%] h-full flex justify-center items-center">
+                <div className="w-[400px] h-[500px] rounded-3xl backdrop-blur-md shadow-2xl flex flex-col justify-center items-center">
+                    <input
+                        onChange={
+                            (e) => {
+                                setEmail(e.target.value)
+                            }}
+                        className="my-[10px] w-[300px] h-[50px] 
+                     border border-[#c3efe9] rounded-2xl"
+                    />
+
+                    <input
+                        onChange={
+                            (e) => {
+                                setPassword(e.target.value)
+                            }}
+                        className="my-[10px] w-[300px] h-[50px] border
+                      border-[#c3efe9]  rounded-2xl" type="password"
+                    />
+
+                    <button
+                        onClick={handleLogin}
+                        className="cursor-pointer my-[10px] w-[300px] h-[50px] 
+                    border border-[#c3efe9] rounded-2xl font-bold
+                     text-white text-[25px]">
+                        LogIn
+                    </button>
+
+                </div>
+
+            </div>
         </div>
     )
 }
